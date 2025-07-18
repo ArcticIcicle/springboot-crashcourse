47ab123970faf5e9ed30f3ba22a55dd94217ff74
ArcticIcicle <cjnchoa@gmail.com>
2025-07-18 17:41:40 +0800

    add: hashcode testcases, equals testcases

--- session2/lab6/myfirstapi/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
+++ session2/lab6/myfirstapi/src/test/java/com/example/demo/myfirstapi/model/ProductTest.java
@@ -56,19 +56,84 @@ class ProductTest {
         assertEquals("Product{id=1, name='Banana', price=150.0}", actual);
     }
 
+    // equals
     @Test
     void shouldBeEqual(){
         Product a = new Product(2L, "Watermelon", 250);
         Product b = new Product(2L, "Watermelon", 250);
-        Boolean actual = a.equals(b);
-        assertEquals(true, actual);
+        boolean actual = a.equals(b);
+        assertTrue(actual);
     }
 
     @Test
     void shouldNotBeEqual(){
         Product a = new Product(2L, "Watermelon", 250);
-        Boolean actual = a.equals(product);
-        assertEquals(false, actual);
+        boolean actual = a.equals(product);
+        assertFalse(actual);
     }
 
+    @Test
+    void shouldNotBeEqualNullObject(){
+        assertFalse(product.equals(null));
+    }
+
+    @Test
+    void shouldNotBeEqualDifferentProductID(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(3L, "Watermelon", 250);
+        boolean actual = a.equals(b);
+        assertFalse(actual);
+    }
+
+    @Test
+    void shouldNotBeEqualDifferentProductName(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(2L, "Peach", 250);
+        boolean actual = a.equals(b);
+        assertFalse(actual);
+    }
+
+    @Test
+    void shouldNotBeEqualDifferentProductPrice(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(2L, "Watermelon", 150);
+        boolean actual = a.equals(b);
+        assertFalse(actual);
+    }
+
+    // hashcode
+    @Test
+    void sameObjectShouldHaveSameHashCode(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(2L, "Watermelon", 250);
+        assertEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    void shouldNotHaveSameHashCode(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(3L, "Peach", 300);
+        assertNotEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    void ShouldNotHaveTheSameHashCodeDifferentProductPrice(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(2L, "Watermelon", 300);
+        assertNotEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    void shouldNotHaveTheSameHashCodeDifferentProductID(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(3L, "Watermelon", 250);
+        assertNotEquals(a.hashCode(), b.hashCode());
+    }
+
+    @Test
+    void shouldNotHaveTheSameHashCodeDifferentProductName(){
+        Product a = new Product(2L, "Watermelon", 250);
+        Product b = new Product(2L, "Peach", 250);
+        assertNotEquals(a.hashCode(), b.hashCode());
+    }
 }
